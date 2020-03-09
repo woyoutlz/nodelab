708b6cc5bc4cedeae00518431f07ef381641e42d
woyoutlz <yundkyy@gmail.com>
2020-03-09 23:21:08 +0800

    new server

--- packages/words/server.js
+++ packages/words/server.js
@@ -1,13 +1,17 @@
 const express = require('express')
+const bodyParser = require('body-parser');
 const app = express()
 const http = require('http').Server(app)
 const config = require("config")
 let port = config.port || 3360
 let host = config.host || "127.0.0.1"
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json())
 app.get("/call",function(req,res){
   res.send("use post")
 })
 app.post("/call",function(req,res){
+  console.log(req.body)
   res.send("hello1")
 })
 app.use((err, req, res, next) => {
