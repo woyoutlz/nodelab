9714cb166959e33327fa9ab3a5965580ec6c20bc
woyoutlz <yundkyy@gmail.com>
2020-03-09 23:02:25 +0800

    remove useless,keep simple

--- packages/words/route/index.js
+++
@@ -1,7 +0,0 @@
-const routerFactory = require('express-promise-router')
-
-/**
- * @param {Express} app
- */
-const v1Router = routerFactory()
-module.exports = v1Router
--- packages/words/server.js
+++ packages/words/server.js
@@ -1,16 +1,20 @@
-const server = require('./lib/server')
-const Logger = require('./lib/logger')
-const route = require('./route')
-const sub = require('./sub')
-let config = require('config')
-server.g.logger = new Logger('word', 'server')
-async function main(){
-  // sub init
-  await sub.init()
-
-  // start services
-  server.app.use('/', route)
-  // route要在start前
-  server.start(config.host,config.port)
-}
-main()
+const express = require('express')
+const app = express()
+const http = require('http').Server(app)
+const config = require("config")
+let port = config.port || 3360
+let host = config.host || "127.0.0.1"
+app.get("/call",function(req,res){
+  res.send("use post")
+})
+app.post("/call",function(req,res){
+  res.send("hello1")
+})
+app.use((err, req, res, next) => {
+  const status = err.status || 500
+  if (status == 500) {
+    g.logger.error(err)
+  }
+  res.status(status).json({ 'code': 1, status: status, msg: err.message })
+})
+http.listen(port, host, () => console.log("[httpserver]",`port=${port}`))
